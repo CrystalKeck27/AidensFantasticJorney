6c3f149ff380a8e1b59623236de97fbb32d49fcd
CrystalKeck27 <45602803+CrystalKeck27@users.noreply.github.com>
2020-03-17 14:40:15 -0500

    One thing I missed in configuring mod data

--- src/main/java/com/aidenkeck/aidensfantasticjorney/AidensFantasticJorney.java
+++ src/main/java/com/aidenkeck/aidensfantasticjorney/AidensFantasticJorney.java
@@ -54,7 +54,7 @@ public class AidensFantasticJorney
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo("aidensfantasticjorney", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
