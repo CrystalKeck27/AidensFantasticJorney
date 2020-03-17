5410cd95a59bc2f429ec30b233e6451d57f84463
CrystalKeck27 <45602803+CrystalKeck27@users.noreply.github.com>
2020-03-17 14:38:11 -0500

    Configure Mod Data

--- src/main/java/com/aidenkeck/aidensfantasticjorney/AidensFantasticJorney.java
+++ src/main/java/com/aidenkeck/aidensfantasticjorney/AidensFantasticJorney.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.aidenkeck.aidensfantasticjorney;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,13 +19,13 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("aidensfantasticjorney")
+public class AidensFantasticJorney
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public AidensFantasticJorney() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
